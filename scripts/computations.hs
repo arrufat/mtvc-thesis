c050be7c4ae25b9a5b4e3f5052e9f76f4cb7598b
Adrià Arrufat <swiftscythe@gmail.com>
2015-03-17 16:48:19 +0100

    Simplified types

--- scripts/computations.hs
+++ scripts/computations.hs
@@ -3,8 +3,8 @@ main = do
         let videoTags = ["Width", "Height", "Frame Rate", "Channels", "Bits Per Channel", "Duration (s)"]
             videoInfo = [1920, 1080, 25, 3, 8, 2 * 3600] :: [Integer]
             videoPrint = zipWith (\t i -> t ++ ": " ++ show i) videoTags videoInfo
-            size = product videoInfo
+            size = fromInteger $ product videoInfo :: Double
         mapM_ putStrLn videoPrint
-        putStrLn $ "Size in bits: " ++ show size ++ " = " ++ show (fromInteger size :: Double)
-        putStrLn $ "Size in Gbits: " ++ show (fromInteger size / (1000 * 1000 * 1000) :: Double)
-        putStrLn $ "Size in GiB: " ++ show (fromInteger size / 8 / (1024 * 1024 * 1024) :: Double)
+        putStrLn $ "Size in bits: " ++ show size ++ " = " ++ show size
+        putStrLn $ "Size in Gbits: " ++ show (size / (1000 * 1000 * 1000))
+        putStrLn $ "Size in GiB: " ++ show (size / 8 / (1024 * 1024 * 1024))
