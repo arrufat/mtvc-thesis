ac527b0a4c418a8b467a95819d6f95672ee70539
Adrià Arrufat <swiftscythe@gmail.com>
2015-04-11 16:46:47 +0200

    Moved figures generation script to scripts dir

--- scripts/pgf2pdf.sh
+++ scripts/pgf2pdf.sh
@@ -8,13 +8,13 @@ TMPFILE=tmp.tex
 echo "\\documentclass[11pt,a4paper]{standalone}"                                    > ${TMPFILE}
 echo "\\usepackage{geometry}"                                                      >> ${TMPFILE}
 echo "\\geometry{verbose,tmargin=2.5cm,bmargin=2.5cm,lmargin=2.5cm,rmargin=2.5cm}" >> ${TMPFILE}
-echo "\\usepackage[usenames,dvipsnames]{xcolor}"                                   >> ${TMPFILE}
-echo "\\usepackage{pgfplots,tikz}"                                                 >> ${TMPFILE}
-echo "\\usetikzlibrary{decorations.markings}"                                      >> ${TMPFILE}
 echo "\\usepackage{amsmath, amssymb}"                                              >> ${TMPFILE}
 echo "\\usepackage[T1]{fontenc}"                                                   >> ${TMPFILE}
 echo "\\usepackage{mathptmx}"                                                      >> ${TMPFILE}
 echo "\\usepackage[scaled]{helvet}"                                                >> ${TMPFILE}
+echo "\\usepackage[usenames,dvipsnames]{xcolor}"                                   >> ${TMPFILE}
+echo "\\usepackage{pgfplots,tikz}"                                                 >> ${TMPFILE}
+echo "\\usetikzlibrary{decorations.markings}"                                      >> ${TMPFILE}
 echo "\\begin{document}"                                                           >> ${TMPFILE}
 echo "\\input{${PGFFILE}}"                                                         >> ${TMPFILE}
 echo "\\end{document}"                                                             >> ${TMPFILE}
