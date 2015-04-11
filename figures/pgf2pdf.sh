a1a864824c3d58966185fe5133581a3d1a409001
Adrià Arrufat <swiftscythe@gmail.com>
2015-04-11 15:52:10 +0200

    CH2: deleted old pgf file, renamed to tex

--- figures/pgf2pdf.sh
+++ figures/pgf2pdf.sh
@@ -5,7 +5,7 @@ PDFFILE=`echo $PGFFILE | sed -e 's/\.tex/\.pdf/g'`
 TMPFILE=tmp.tex
 
 # wrap the PGF file inside a LaTeX document
-echo "\\documentclass[11pt,a4paper]{standalone}"                                   > ${TMPFILE}
+echo "\\documentclass[11pt,a4paper]{standalone}"                                    > ${TMPFILE}
 echo "\\usepackage{geometry}"                                                      >> ${TMPFILE}
 echo "\\geometry{verbose,tmargin=2.5cm,bmargin=2.5cm,lmargin=2.5cm,rmargin=2.5cm}" >> ${TMPFILE}
 echo "\\usepackage[usenames,dvipsnames]{xcolor}"                                   >> ${TMPFILE}
