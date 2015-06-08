4d8c4dac3107cdf73be23e3e02558e79799835fa
Adrià Arrufat <swiftscythe@gmail.com>
2015-06-08 17:12:01 +0200

    REV: adopted (some) Pierrick suggestions
    Also continued working on chapter 3

--- scripts/computations.hs
+++ scripts/computations.hs
@@ -1,10 +1,9 @@
 main :: IO ()
 main = do
-        let videoTags = ["Width", "Height", "Frame Rate", "Channels", "Bits Per Channel", "Duration (s)"]
-            videoInfo = [1920, 1080, 25, 3, 8, 2 * 3600] :: [Integer]
+        let videoTags = ["Width", "Height", "Frame Rate", "Channels", "Bits Per Channel"]
+            videoInfo = [1920, 1080, 25, 3, 8] :: [Integer]
             videoPrint = zipWith (\t i -> t ++ ": " ++ show i) videoTags videoInfo
             size = fromInteger $ product videoInfo :: Double
         mapM_ putStrLn videoPrint
-        putStrLn $ "Size in bits: " ++ show size ++ " = " ++ show size
-        putStrLn $ "Size in Gbits: " ++ show (size / (1000 * 1000 * 1000))
-        putStrLn $ "Size in GiB: " ++ show (size / 8 / (1024 * 1024 * 1024))
+        putStrLn $ "Rate in bits/s: " ++ show size ++ " = " ++ show size
+        putStrLn $ "Rate in Gbits/s: " ++ show (size / (1000 * 1000 * 1000))
