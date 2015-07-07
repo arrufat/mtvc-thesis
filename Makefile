a7a9d527dac70ac6fd232e3e057425c9ebe82414
Adrià Arrufat <swiftscythe@gmail.com>
2015-07-07 16:50:16 +0200

    GENERAL: Improved the figures build system

--- Makefile
+++ Makefile
@@ -4,8 +4,8 @@ TEXC		= latexmk
 TEXFLAGS	= -lualatex --shell-escape -synctex=1 -quiet
 SOURCE		= $(shell grep -l '\\documentclass' *.tex)
 OBJECTS		= $(SOURCE:.tex=.bbl) $(SOURCE:.tex=.nav) $(SOURCE:.tex=.snm) $(SOURCE:.tex=.synctex.gz) $(SOURCE:.tex=.ptc)
-MKPLOTS		= ./scripts/plot2pdf.sh
-PLOTS		= $(wildcard figures/*plot.tex)
+MKFIGURES	= ./scripts/fig2pdf.sh
+FIGURES		= $(wildcard figures/*.tex)
 REL_NAME	= mdtc_thesis
 
 .PHONY: figures
@@ -15,8 +15,8 @@ default: build
 build: $(SOURCE)
 	$(TEXC) $(TEXFLAGS) $(SOURCE)
 
-plots: $(plots)
-	$(foreach plot,$(PLOTS), $(MKPLOTS) $(plot);)
+figures: $(figures)
+	$(foreach fig,$(FIGURES), $(MKFIGURES) $(fig);)
 
 clean:
 	$(TEXC) -c
--- scripts/fig2pdf.sh
+++ scripts/fig2pdf.sh
@@ -6,15 +6,18 @@ TMPFILE=tmp.tex
 
 # wrap the PGF file inside a LaTeX document
 echo "\\documentclass[11pt,a4paper]{standalone}"                                    > ${TMPFILE}
+if [[ ${PGFFILE} == *"_plot.tex" ]]; then
 echo "\\usepackage{geometry}"                                                      >> ${TMPFILE}
 echo "\\geometry{verbose,tmargin=2.5cm,bmargin=2.5cm,lmargin=2.5cm,rmargin=2.5cm}" >> ${TMPFILE}
+fi
 echo "\\usepackage{amsmath, amssymb}"                                              >> ${TMPFILE}
 echo "\\usepackage[T1]{fontenc}"                                                   >> ${TMPFILE}
 echo "\\usepackage{mathptmx}"                                                      >> ${TMPFILE}
 echo "\\usepackage[scaled]{helvet}"                                                >> ${TMPFILE}
 echo "\\usepackage[usenames,dvipsnames]{xcolor}"                                   >> ${TMPFILE}
+echo "\\usepackage{ifthen}"                                                        >> ${TMPFILE}
 echo "\\usepackage{pgfplots,tikz}"                                                 >> ${TMPFILE}
-echo "\\usetikzlibrary{decorations.markings,intersections}"                        >> ${TMPFILE}
+echo "\\usetikzlibrary{shapes,arrows,fit,calc,decorations.markings,intersections}" >> ${TMPFILE}
 echo "\\usepgfplotslibrary{fillbetween}"                                           >> ${TMPFILE}
 echo "\\pgfplotsset{compat=1.12}"                                                  >> ${TMPFILE}
 echo "\\begin{document}"                                                           >> ${TMPFILE}
