de0010c3e418b5e804a2f1b264f7aa966f81282c
Adrià Arrufat <swiftscythe@gmail.com>
2015-07-07 16:10:50 +0200

    Revert "PLOT2PDF: fixed includes and excessive cropping"
    This reverts commit 496d8e23291bbb60fe0b35bfc21b9e03bd5cd430.

--- scripts/plot2pdf.sh
+++ scripts/plot2pdf.sh
@@ -5,13 +5,14 @@ PDFFILE=`echo $PGFFILE | sed -e 's/\.tex/\.pdf/g'`
 TMPFILE=tmp.tex
 
 # wrap the PGF file inside a LaTeX document
-echo "\\documentclass[11pt]{standalone}"                                    > ${TMPFILE}
+echo "\\documentclass[11pt,a4paper]{standalone}"                                    > ${TMPFILE}
+echo "\\usepackage{geometry}"                                                      >> ${TMPFILE}
+echo "\\geometry{verbose,tmargin=2.5cm,bmargin=2.5cm,lmargin=2.5cm,rmargin=2.5cm}" >> ${TMPFILE}
 echo "\\usepackage{amsmath, amssymb}"                                              >> ${TMPFILE}
 echo "\\usepackage[T1]{fontenc}"                                                   >> ${TMPFILE}
 echo "\\usepackage{mathptmx}"                                                      >> ${TMPFILE}
 echo "\\usepackage[scaled]{helvet}"                                                >> ${TMPFILE}
 echo "\\usepackage[usenames,dvipsnames]{xcolor}"                                   >> ${TMPFILE}
-echo "\\usepackage{ifthen}"                                                        >> ${TMPFILE}
 echo "\\usepackage{pgfplots,tikz}"                                                 >> ${TMPFILE}
 echo "\\usetikzlibrary{decorations.markings,intersections}"                        >> ${TMPFILE}
 echo "\\usepgfplotslibrary{fillbetween}"                                           >> ${TMPFILE}
