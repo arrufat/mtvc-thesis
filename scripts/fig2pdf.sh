aa07ea8a807c52e5be283ea244823f99fe9aae31
Adrià Arrufat <swiftscythe@gmail.com>
2015-07-08 22:02:31 +0200

    FIG2PDF: Handle many files at a time

--- scripts/fig2pdf.sh
+++ scripts/fig2pdf.sh
@@ -1,32 +1,36 @@
 #!/usr/bin/env bash
 TEXFLAGS='-lualatex --shell-escape -quiet'
-PGFFILE=$1
-PDFFILE=`echo $PGFFILE | sed -e 's/\.tex/\.pdf/g'`
 TMPFILE=tmp.tex
 
-# wrap the PGF file inside a LaTeX document
-echo "\\documentclass[11pt,a4paper]{standalone}"                                    > ${TMPFILE}
-if [[ ${PGFFILE} == *"_plot.tex" ]]; then
-echo "\\usepackage{geometry}"                                                      >> ${TMPFILE}
-echo "\\geometry{verbose,tmargin=2.5cm,bmargin=2.5cm,lmargin=2.5cm,rmargin=2.5cm}" >> ${TMPFILE}
-fi
-echo "\\usepackage{amsmath, amssymb}"                                              >> ${TMPFILE}
-echo "\\usepackage[T1]{fontenc}"                                                   >> ${TMPFILE}
-echo "\\usepackage{mathptmx}"                                                      >> ${TMPFILE}
-echo "\\usepackage[scaled]{helvet}"                                                >> ${TMPFILE}
-echo "\\usepackage[usenames,dvipsnames]{xcolor}"                                   >> ${TMPFILE}
-echo "\\usepackage{ifthen}"                                                        >> ${TMPFILE}
-echo "\\usepackage{pgfplots,tikz}"                                                 >> ${TMPFILE}
-echo "\\usetikzlibrary{shapes,arrows,fit,calc,decorations.markings,intersections}" >> ${TMPFILE}
-echo "\\usepgfplotslibrary{fillbetween}"                                           >> ${TMPFILE}
-echo "\\pgfplotsset{compat=1.12}"                                                  >> ${TMPFILE}
-echo "\\begin{document}"                                                           >> ${TMPFILE}
-echo "\\input{${PGFFILE}}"                                                         >> ${TMPFILE}
-echo "\\end{document}"                                                             >> ${TMPFILE}
+for f in ${@}
+do
+	PGFFILE=$f
+	PDFFILE=`echo $PGFFILE | sed -e 's/\.tex/\.pdf/g'`
 
-echo "Generating figure ${PDFFILE}"
-latexmk ${TEXFLAGS} ${TMPFILE}
-mv tmp.pdf ${PDFFILE}
-latexmk -c
-rm ${TMPFILE}
-echo
+	# wrap the PGF file inside a LaTeX document
+	echo "\\documentclass[11pt,a4paper]{standalone}"                                    > ${TMPFILE}
+	if [[ ${PGFFILE} == *"_plot.tex" ]]; then
+	echo "\\usepackage{geometry}"                                                      >> ${TMPFILE}
+	echo "\\geometry{verbose,tmargin=2.5cm,bmargin=2.5cm,lmargin=2.5cm,rmargin=2.5cm}" >> ${TMPFILE}
+	fi
+	echo "\\usepackage{amsmath, amssymb}"                                              >> ${TMPFILE}
+	echo "\\usepackage[T1]{fontenc}"                                                   >> ${TMPFILE}
+	echo "\\usepackage{mathptmx}"                                                      >> ${TMPFILE}
+	echo "\\usepackage[scaled]{helvet}"                                                >> ${TMPFILE}
+	echo "\\usepackage[usenames,dvipsnames]{xcolor}"                                   >> ${TMPFILE}
+	echo "\\usepackage{ifthen}"                                                        >> ${TMPFILE}
+	echo "\\usepackage{pgfplots,tikz}"                                                 >> ${TMPFILE}
+	echo "\\usetikzlibrary{shapes,arrows,fit,calc,decorations.markings,intersections}" >> ${TMPFILE}
+	echo "\\usepgfplotslibrary{fillbetween}"                                           >> ${TMPFILE}
+	echo "\\pgfplotsset{compat=1.12}"                                                  >> ${TMPFILE}
+	echo "\\begin{document}"                                                           >> ${TMPFILE}
+	echo "\\input{${PGFFILE}}"                                                         >> ${TMPFILE}
+	echo "\\end{document}"                                                             >> ${TMPFILE}
+
+	echo "Generating figure ${PDFFILE}"
+	latexmk ${TEXFLAGS} ${TMPFILE}
+	mv tmp.pdf ${PDFFILE}
+	latexmk -c
+	rm ${TMPFILE}
+	echo
+done
