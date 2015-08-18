f97d8f59d1d09bab0e9a5f37526dd545eecf60a9
Adrià Arrufat <swiftscythe@gmail.com>
2015-08-18 18:59:47 +0200

    Added computations for combinations

--- scripts/computations.hs
+++ scripts/computations.hs
@@ -3,7 +3,15 @@ main = do
         let videoTags = ["Width", "Height", "Frame Rate", "Channels", "Bits Per Channel"]
             videoInfo = [1920, 1080, 25, 3, 8] :: [Integer]
             videoPrint = zipWith (\t i -> t ++ ": " ++ show i) videoTags videoInfo
-            size = fromInteger $ product videoInfo :: Double
+            size = product videoInfo
         mapM_ putStrLn videoPrint
-        putStrLn $ "Rate in bits/s: " ++ show size ++ " = " ++ show size
-        putStrLn $ "Rate in Gbits/s: " ++ show (size / (1000 * 1000 * 1000))
+        putStrLn $ "Rate in bits/s: " ++ show size ++ " = " ++ show (fromInteger size :: Double)
+        putStrLn $ "Rate in Gbits/s: " ++ show (fromInteger size / (1000 * 1000 * 1000) :: Double)
+        let comb = combinations 256 4
+        putStrLn $ "Combinations 256 over 5: " ++ show (comb :: Integer) ++ " = " ++ show (fromInteger comb :: Double)
+
+factorial :: (Enum a, Num a) => a -> a
+factorial n = product [1..n]
+
+combinations :: Integral a => a -> a -> a
+combinations n k = factorial n `div` (factorial k * factorial (n - k))
